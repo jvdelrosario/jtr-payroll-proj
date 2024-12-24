19122da42dceccdf4760192bb0c6aec866c8e30b
JV Del Rosario <Jvdelrosario101@gmail.com>
2024-12-25 02:03:53 +0800

    fixed the problem regarding the tailwind configuration

--- generic-payroll/tailwind.config.ts
+++ generic-payroll/tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./styles/**/*.{css,js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{css,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
